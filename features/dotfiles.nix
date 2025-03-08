408cc61d6d9769efa6fc68913bf03592882ffa2b
Jan Baer <jan@janbaer.de>
2025-03-08 17:31:41 +0100

    Configure Zsh with using dotfiles and p1ok

--- features/dotfiles.nix
+++ features/dotfiles.nix
@@ -9,6 +9,7 @@ in
     ".config/nvim".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/nvim";
     ".config/atuin".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/atuin";
     ".p10k.zsh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.p10k.zsh";
+    ".config/zsh/.functions".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/zsh/.functions";
     ".fzf-init.zsh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.fzf-init.zsh";
     "bin/init-keychain.sh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/bin/init-keychain.sh";
   };
--- features/zsh.nix
+++ features/zsh.nix
@@ -1,18 +1,50 @@
-{pkgs, ...}: {
+{ config, pkgs, ... }: {
 
   programs.zsh = {
     enable = true;
     autosuggestion = {
       enable = true;
-      highlight= "fg=#ff00ff,bold,underline";
+      highlight = "fg=#ff00ff,bold,underline";
     };
     enableCompletion = true; # https://mynixos.com/home-manager/option/programs.zsh.enableCompletion
     dotDir = ".config/zsh";
     initExtra = ''
+      # Source local zshrc with local only settings
+      [[ -f $ZDOTDIR/.zshrc.local ]] && source $ZDOTDIR/.zshrc.local
+
+      [[ -f $HOME/.p10k.zsh ]] && source $HOME/.p10k.zsh
+
+      [ -f $HOME/.fzf-init.zsh ] && source $HOME/.fzf-init.zsh
+
+      # Config keys for Atuin together with Fzf and run init for Zsh
+      [ -f $HOME/.config/atuin/atuin-setup.sh ] && source $HOME/.config/atuin/atuin-setup.sh
+
+      [ -f $HOME/.cargo/env ] && source $HOME/.cargo/env
+
+      # Some kubernetes things
+      [ -f $HOME/.kube/kube-config.yaml ] && export KUBECONFIG=$HOME/.kube/kube-config.yaml
+
+      # Source local zshrc with local bu specific settings, if file exists
+      [ -f $ZDOTDIR/.zshrc.bu ] && source $ZDOTDIR/.zshrc.bu
+
+      export KEYCHAIN_KEYS="$KEYCHAIN_KEYS_LOCAL $KEYCHAIN_KEYS_BU"
+      [ -f $HOME/tmp/keychain_init_done ] && source $HOME/bin/init-keychain.sh
+
       if type zoxide &>/dev/null; then
         eval "$(zoxide init zsh --cmd cd)"
       fi
+
+      source $ZDOTDIR/.functions
+
+      [ -f $ZDOTDIR/.zsh-secrets ] && source $ZDOTDIR/.zsh-secrets
     '';
+    plugins = [
+      {
+        name = "zsh-powerlevel10k";
+        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+        file = "powerlevel10k.zsh-theme";
+      }
+    ];
     shellAliases = {
       l = "lf";
       lg = "lazygit";
@@ -21,6 +53,10 @@
       ".." = "cd ..";
       "..." = "cd ../..";
       "...." = "cd ../../..";
+      reload = "source $ZDOTDIR/.zshrc";
+      kc = "source $HOME/bin/init-keychain.sh";
+      g = "git";
+      gfp = "git fetch --prune && git pull";
     };
     sessionVariables = {
       EDITOR = "nvim";
