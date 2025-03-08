865fed08321c3e95755583546dade32ca6b9475b
Jan Baer <jan@janbaer.de>
2025-03-08 17:01:56 +0100

    Remove cloning dotfiles project here
    It's better to fetch it not in this way, since it's part of the Nix
    profile and would be immutable

--- features/dotfiles.nix
+++ features/dotfiles.nix
@@ -5,21 +5,11 @@ in
 {
 
   home.file = {
-    "Projects/dotfiles" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "janbaer";
-        repo = "dotfiles";
-        tag = "2025-03-01";
-        sha256 = "uB7yxbYzvQsOKDNss1B/wqEzM9q+mTDA6WEao6aicKM=";
-      };
-      recursive = true;
-    };
-  };
-
-  home.file = {
     ".config/lazygit".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/lazygit";
     ".config/nvim".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/nvim";
     ".config/atuin".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.config/atuin";
     ".p10k.zsh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.p10k.zsh";
+    ".fzf-init.zsh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/.fzf-init.zsh";
+    "bin/init-keychain.sh".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/dotfiles/bin/init-keychain.sh";
   };
 }
