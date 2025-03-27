8dd3c7b881ebd19b4b3e102543971e7d597b627f
Jan Baer <jan@janbaer.de>
2025-03-27 16:48:54 +0100

    Fix problem with wrong owner of files created from secrets

--- hosts/common/secrets.nix
+++ hosts/common/secrets.nix
@@ -8,18 +8,21 @@
         path = "/home/${username}/.config/zsh/.zsh-secrets";
         owner = "${username}";
         mode = "0600";
+        symlink = false;
       };
       "id_ed25519" = {
         file = ../../secrets/id_ed25519.age;
         path = "/home/${username}/.ssh/id_ed25519";
         owner = "${username}";
         mode = "0600";
+        symlink = false;
       };
       "id_ed25519_forgejo" = {
         file = ../../secrets/id_ed25519_forgejo.age;
         path = "/home/${username}/.ssh/id_ed25519_forgejo";
         owner = "${username}";
         mode = "0600";
+        symlink = false;
       };
     };
   };
