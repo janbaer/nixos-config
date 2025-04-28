e27720c6950461c1ad006a2950f28cfad8f9c07e
Jan Baer <jan@janbaer.de>
2025-04-28 21:27:32 +0200

    Change IP addresses for local systems in /etc/hosts file

--- modules/nixos/network-hosts.nix
+++ modules/nixos/network-hosts.nix
@@ -8,7 +8,8 @@ in
   config = mkIf cfg.enable {
     networking = {
       hosts = {
-        "192.168.178.7" = [ "forgejo" ];
+        "192.168.20.3" = [ "jabasoft-pve" ];
+        "192.168.20.7" = [ "forgejo" ];
       };
     };
   };
