c718846249256f9859d6a1d066d9012bd2633f30
Jan Baer <jan@janbaer.de>
2025-05-16 20:19:51 +0200

    Use explicit version in the url for the tuxedo drivers
    This is to make sure, that the SHA256 will always fit to the package
    that should be downloaded. Just using master is not safe

--- modules/nixos/tuxedo.nix
+++ modules/nixos/tuxedo.nix
@@ -3,7 +3,7 @@ with lib; let
   cfg = config.modules.openssh;
   # https://flakehub.com/flake/sund3RRR/tuxedo-nixos?view=readme
   tuxedo = import (builtins.fetchTarball {
-    url = "https://github.com/sund3RRR/tuxedo-nixos/archive/master.tar.gz";
+    url = "https://github.com/sund3RRR/tuxedo-nixos/archive/refs/tags/v1.0.1.tar.gz";
     sha256 = "sha256:136wb97skfbx20asc5b4zy8cxh6shs0bwys3nyac463s7yq59vhh";
   });
 in
@@ -22,7 +22,6 @@ in
       tailor-gui.enable = true;
     };
     hardware.tuxedo-drivers.enable = true;
-
     hardware.tuxedo-control-center.enable = true;
   };
 }
