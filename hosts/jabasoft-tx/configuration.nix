fda3bfa6804ff8694f1f09936257fcf1c27862ac
Jan Baer <jan@janbaer.de>
2025-05-17 09:52:01 +0200

    Activate Bluetooth support
    https://nixos.wiki/wiki/Bluetooth

--- hosts/jabasoft-tx/configuration.nix
+++ hosts/jabasoft-tx/configuration.nix
@@ -65,6 +65,11 @@ in {
     variant = "";
   };
 
+  # Enable bluetooth support
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
