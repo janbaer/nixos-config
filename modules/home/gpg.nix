81ba0a65a1f6e71b28c72c096a4f209b452759e9
Jan Baer <jan@janbaer.de>
2025-05-18 20:06:27 +0200

    Install Keychain with custom version (overrideAttrs) and copy gpg-agent.conf

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -1,30 +1,38 @@
-{ config, lib, pkgs, username, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
-let cfg = config.modules.gpg;
+let
+  cfg = config.modules.gpg;
+  keychain = pkgs.keychain.overrideAttrs {
+    version = "2.9.5";
+    src = pkgs.fetchurl {
+      url = "https://github.com/funtoo/keychain/archive/refs/tags/2.9.5.tar.gz";
+      hash = "sha256-yIPybbYWvByBul7zgyx62RLz6L0Lr2qv+YEWTFOKFBE=";
+    };
+  };
 in {
   options.modules.gpg.enable = mkEnableOption "Configuration of GPG";
 
   config = mkIf cfg.enable {
     home.file = {
-      ".gnupg/public-key.gpg".text = builtins.readFile ./../../secrets/public-gpg-key.gpg;
-      ".gnupg/gpg-agent.conf".text = ''
-        use-agent
-        allow-loopback-pinentry
-      '';
+    #   ".gnupg/public-key.gpg".text = builtins.readFile ./../../secrets/public-gpg-key.gpg;
+    ".gnupg/gpg-agent.conf".text = builtins.readFile ./files/gpg-agent.conf;
     };
-
-    home.packages = with pkgs; [
-      seahorse # Application for managing encryption keys and passwords in the GnomeKeyring
-    ];
 
     programs.gpg = { enable = true; };
 
-    home.activation = {
-      import_gpg_keys = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        /etc/profiles/per-user/${username}/bin/gpg --batch --import ~/.gnupg/private-key.gpg
-        /etc/profiles/per-user/${username}/bin/gpg --batch --import ~/.gnupg/public-key.gpg
-        rm ~/.gnupg/*.gpg
-      '';
-    };
+    home.packages = with pkgs; [
+      # gnupg     # Modern release of the GNU Privacy Guard, a GPL OpenPGP implementation
+      seahorse    # Application for managing encryption keys and passwords in the GnomeKeyring
+      keychain    # Keychain management tool for SSH and GPG keys -- deactivated since I need newer version
+    ];
+
+
+    # home.activation = {
+    #   import_gpg_keys = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    #     /etc/profiles/per-user/${username}/bin/gpg --batch --import ~/.gnupg/private-key.gpg
+    #     /etc/profiles/per-user/${username}/bin/gpg --batch --import ~/.gnupg/public-key.gpg
+    #     rm ~/.gnupg/*.gpg
+    #   '';
+    # };
   };
 }
