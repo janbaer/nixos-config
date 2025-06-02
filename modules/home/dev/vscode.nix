ffb0379c1b11d9d689b7546319f5ca4fe8f8779e
Jan Baer <jan@janbaer.de>
2025-06-02 16:29:59 +0200

    Dev: Reactivate installation of VSCode extensions and configure default profile

--- modules/home/dev/vscode.nix
+++ modules/home/dev/vscode.nix
@@ -12,28 +12,42 @@ in
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-    #   profiles = {
-    #     default = {
-    #       userSettings = { };
-    #
-    #       keybindings = [
-    #
-    #       ];
-    #
-    #       # https://mynixos.com/search?q=vscode-extensions
-    #       extensions = with pkgs.vscode-extensions; [
-    #         golang.go
-    #         dracula-theme.theme-dracula
-    #         enkia.tokyo-night
-    #         redhat.vscode-yaml
-    #         redhat.ansible
-    #         bbenoist.nix
-    #         jnoortheen.nix-ide
-    #         vscodevim.vim
-    #         hashicorp.hcl
-    #       ];
-    #     };
-    #   };
+      profiles = {
+        default = {
+          userSettings = {
+            diffEditor = {
+              ignoreTrimWhitespace= false;
+            };
+            editor = {
+              bracketPairColorization.enabled = true;
+              guides.bracketPairs = "active";
+            };
+            go.useLanguageServer = true;
+            window.zoomLevel= 1;
+            workbench = {
+              colorTheme = "Dracula Theme";
+              statusBar.visible = true;
+              settings.editor = "ui";
+            };
+          };
+
+          keybindings = [
+          ];
+
+          # https://mynixos.com/search?q=vscode-extensions
+          extensions = with pkgs.vscode-extensions; [
+            golang.go
+            dracula-theme.theme-dracula
+            enkia.tokyo-night
+            redhat.vscode-yaml
+            redhat.ansible
+            bbenoist.nix
+            jnoortheen.nix-ide
+            vscodevim.vim
+            hashicorp.hcl
+          ];
+        };
+      };
     };
   };
 }
