23331fefcb8a19e80a4beb832baecc8d711b7253
Jan Baer <jan@janbaer.de>
2025-06-07 18:01:54 +0200

    Users: read public SSH key from local file, also create group jan

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,22 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  pubSSHKey = builtins.readFile ../../secrets/id_ed25519.pub;
+in {
 
   users = {
+    groups = {
+      jan = {
+        gid = 1000;
+      };
+    };
     users = {
       jan = {
         isNormalUser = true;
         description = "Jan Baer";
-        extraGroups = [ "networkmanager" "wheel" "ssh-users" ];
-        # group = "jan";
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZlTGJF57sVlu7Prmm41Y8GmaqpespwCMFB7fLROBSm jan@janbaer.de" ];
-        packages = with pkgs; [];
+        extraGroups = [ "jan" "networkmanager" "wheel" "ssh-users" ];
+        openssh.authorizedKeys.keys = [ pubSSHKey ];
         shell = pkgs.zsh; # Make Zsh as default shell
       };
     };
-    # groups = {
-    #   jan = {
-    #     gid = 1000;
-    #   };
-    # };
   };
 }
 
