9c4b9cb6ce82eb070d2e2da2833bc0ca00888e4d
Jan Baer <jan@janbaer.de>
2025-09-07 19:46:55 +0200

    gnome: Activate preview in Nautilus file manager

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -7,16 +7,17 @@
   ];
 
   home.packages = with pkgs; [
-    nautilus
-    simple-scan
-    keepassxc
-    obsidian
+    nautilus        # File manager for GNOME
+    sushi           # Quick previewer for Nautilus
+    simple-scan     # Simple scanning utility
+    keepassxc       # Offline password manager with many features
+    obsidian        # Powerful knowledge base that works on top of a local folder of plain text Markdown files
     localsend       # Open source cross-platform alternative to AirDrop
     gedit           # Former GNOME text editor
     gthumb          # Image browser and viewer for GNOME
     evince          # GNOME's document viewer
+    papers          # Gnome document viewer (fork from evince)
     filezilla       # Graphical FTP, FTPS and SFTP client
-
     easytag         # View and edit tags for various audio files
   ];
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -33,4 +33,8 @@ in {
     enable = true;
     libraries = [];
   };
+
+  services.gnome = {
+    sushi.enable = true;
+  };
 }
