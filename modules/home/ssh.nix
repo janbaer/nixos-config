c368d91176ff4b56bc6d65fdbd18cc0d6e59e6d4
Jan Baer <jan@janbaer.de>
2025-09-11 10:06:20 +0200

    ssh: Installation sshpass to be able to pass TOTP tokens

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -11,6 +11,10 @@ in {
     ".ssh/id_ed25519_jabasoft-ug.pub".text = builtins.readFile ./../../secrets/id_ed25519_jabasoft-ug.pub;
   };
 
+  home.packages = with pkgs; [
+    sshpass         # Non-interactive ssh password auth
+  ];
+
   programs.ssh = {
     enable = true;
 
