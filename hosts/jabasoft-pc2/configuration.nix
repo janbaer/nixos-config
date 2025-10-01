32ef00cb9860c8db26395b537d88926169228f2c
Jan Baer <jan@janbaer.de>
2025-10-01 06:03:41 +0200

    nixos: Activating support for Flatpak

--- hosts/jabasoft-pc2/configuration.nix
+++ hosts/jabasoft-pc2/configuration.nix
@@ -58,6 +58,8 @@
     xwayland.enable = true;
   };
 
+  services.flatpak.enable = true;
+
   # Enable bluetooth support
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
