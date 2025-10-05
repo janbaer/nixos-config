e2597d5fff87ebaea462c15f3153fc965ced9a69
Jan Baer <jan@janbaer.de>
2025-10-05 18:36:53 +0200

    desktop: Installing rquickshare instead of localsend for easier file sharing with Android devices

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -12,12 +12,12 @@
     simple-scan     # Simple scanning utility
     keepassxc       # Offline password manager with many features
     obsidian        # Powerful knowledge base that works on top of a local folder of plain text Markdown files
-    localsend       # Open source cross-platform alternative to AirDrop
     gedit           # Former GNOME text editor
     gthumb          # Image browser and viewer for GNOME
     papers          # Gnome document viewer (This is the fork and successor of Evince)
     filezilla       # Graphical FTP, FTPS and SFTP client
     easytag         # View and edit tags for various audio files
     pdfarranger     # PDF document editor
+    rquickshare     # Rust implementation of NearbyShare/QuickShare from Android for Linux
   ];
 }
