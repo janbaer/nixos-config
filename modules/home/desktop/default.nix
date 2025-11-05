390a26a260865bcf3d3c0146254b27d3db423895
Jan Baer <jan@janbaer.de>
2025-11-05 05:14:31 +0100

    desktop: Adding gnome-disk-utilities

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -7,17 +7,18 @@
   ];
 
   home.packages = with pkgs; [
-    nautilus        # File manager for GNOME
-    sushi           # Quick previewer for Nautilus
-    simple-scan     # Simple scanning utility
-    keepassxc       # Offline password manager with many features
-    obsidian        # Powerful knowledge base that works on top of a local folder of plain text Markdown files
-    gedit           # Former GNOME text editor
-    gthumb          # Image browser and viewer for GNOME
-    papers          # Gnome document viewer (This is the fork and successor of Evince)
-    filezilla       # Graphical FTP, FTPS and SFTP client
-    easytag         # View and edit tags for various audio files
-    pdfarranger     # PDF document editor
-    rquickshare     # Rust implementation of NearbyShare/QuickShare from Android for Linux
+    nautilus            # File manager for GNOME
+    sushi               # Quick previewer for Nautilus
+    simple-scan         # Simple scanning utility
+    keepassxc           # Offline password manager with many features
+    obsidian            # Powerful knowledge base that works on top of a local folder of plain text Markdown files
+    gedit               # Former GNOME text editor
+    gthumb              # Image browser and viewer for GNOME
+    gnome-disk-utility  # Udisks graphical front-end
+    papers              # Gnome document viewer (This is the fork and successor of Evince)
+    filezilla           # Graphical FTP, FTPS and SFTP client
+    easytag             # View and edit tags for various audio files
+    pdfarranger         # PDF document editor
+    rquickshare         # Rust implementation of NearbyShare/QuickShare from Android for Linux
   ];
 }
