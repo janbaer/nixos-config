af69c80b14827bee6a9f66b4ee6aff50484d2b4a
Jan Baer <jan@janbaer.de>
2026-02-15 18:25:52 +0100

    yazi: Adding new key C-t to create a new tab
    The default t is used for toggle the preview pane

--- modules/home/shell/yazi.nix
+++ modules/home/shell/yazi.nix
@@ -21,6 +21,7 @@ in {
         mgr.prepend_keymap = [
           { on = [ "S" ]; run = "shell \"$SHELL\" --block"; desc = "Open $SHELL here"; }
           { on = [ "<C-d>" ]; run = "shell --confirm 'sudo rm -rf \"$@\"' -- %s"; desc = "Delete selected files with sudo"; }
+          { on = [ "<C-t>" ]; run = "tab_create --current"; desc = "Create a new tab with CWD"; }
           { on = [ "t" ]; run = "plugin toggle-pane min-preview"; desc = "Toggle preview"; }
           { on = [ "T" ]; run = "plugin toggle-pane max-preview"; desc = "Maximize preview"; }
           { on = [ "g" "H" ]; run = "cd ~/"; desc = "Go to home directory"; }
@@ -33,7 +34,7 @@ in {
           { on = [ "g" "m" ]; run = "cd /mnt/zb-data/metube/mp3"; desc = "Go to Metube music"; }
           { on = [ "g" "N" ]; run = "cd /home/${username}/mnt/mailbox-drive/Jan Baer/Notes/"; desc = "Go to Notes"; }
           { on = [ "g" "w" ]; run = "cd /run/media/${username}/MyNotes/Wochenberichte/2026/"; desc = "Go to Wochenberichte"; }
-          { on = [ "g" "x" ]; run = "cd /run/media/${username}/xxx5/"; desc = "Go to xxx5"; }
+          { on = [ "g" "x" ]; run = "cd /run/media/${username}/XXX5/"; desc = "Go to XXX5"; }
           { on = [ "g" "c" ]; run = "cd ~/.config"; desc = "Go to .config"; }
           { on = [ "g" "b" ]; run = "cd ~/bin"; desc = "Go to bin"; }
         ];
