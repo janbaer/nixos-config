44eb9a887994b7fe50c667ed95b963288120f53c
Jan Baer <jan@janbaer.de>
2026-03-18 08:47:45 +0100

    dev: Upgrading Bun to v1.3.11

--- modules/home/dev/bun.nix
+++ modules/home/dev/bun.nix
@@ -8,10 +8,10 @@ let
   cfg = config.modules.dev.bun;
 
   bunOverride = pkgs.bun.overrideAttrs (oldAttrs: rec {
-    version = "1.3.10";
+    version = "1.3.11";
     src = builtins.fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-      sha256 = "sha256:021f7mrppp2lpa0ajcbipvbv51jlvagkhfms2vkksqirgqcc0yzm";
+      sha256 = "sha256:1v876r4v8c31jvpss1sxbmgc29h32qahlx1qdxdg11pqba9vl4c6";
     };
   });
 in
