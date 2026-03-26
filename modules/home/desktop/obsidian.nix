cb99c45f5049eee4781dc3127190c4d02ae70029
Jan Baer <jan@janbaer.de>
2026-03-26 08:19:12 +0100

    desktop: Upgrading Obsidian to v1.12.7

--- modules/home/desktop/obsidian.nix
+++ modules/home/desktop/obsidian.nix
@@ -9,10 +9,10 @@ in {
   config = mkIf cfg.enable {
     home.packages = [
       (pkgs.obsidian.overrideAttrs (_: rec {
-        version = "1.12.4";
+        version = "1.12.7";
         src = pkgs.fetchurl {
           url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/obsidian-${version}.tar.gz";
-          hash = "sha256-cusm388SP44HvoCD90+gRfQAxx7B/mTlirkdnMCEyN4=";
+          hash = "sha256-/L4IsRHZwf2wm5wIlSsG4cgpxiFj66JYTEtOyFm+B50=";
         };
       }))
     ];
