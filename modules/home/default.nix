a471b26563596bb07978e69e5b9f95b9ba7b604e
Jan Baer <jan@janbaer.de>
2026-06-14 20:52:24 +0200

    home: Auto-starting changed user services on activation (sd-switch)

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -10,5 +10,10 @@
     ./ssh.nix
     ./usb-automount.nix
   ];
+
+  # Start/restart changed user services during activation instead of waiting for
+  # the next login, so newly-added services (e.g. the hypridle pre-sleep locker)
+  # come up on `nixos-switch` without a relogin.
+  systemd.user.startServices = "sd-switch";
 }
 
