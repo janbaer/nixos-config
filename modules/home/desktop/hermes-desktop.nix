f80627a5ddb262fe2d75dcfcf121195681eb3540
Jan Baer <jan@janbaer.de>
2026-08-11 15:33:52 +0200

    desktop: Renaming variable to hermesDesktopBinPath

--- modules/home/desktop/hermes-desktop.nix
+++ modules/home/desktop/hermes-desktop.nix
@@ -2,20 +2,20 @@
 with lib;
 let
   cfg = config.modules.desktop.hermes-desktop;
-  bin = "${config.home.homeDirectory}/hermes-desktop/bin/hermes-desktop";
+  hermesDesktopBinPath = "${config.home.homeDirectory}/hermes-desktop/bin/hermes-desktop";
 in {
   options.modules.desktop.hermes-desktop.enable = mkEnableOption "Hermes Agent desktop client";
 
   config = mkIf cfg.enable {
     home.shellAliases = {
-      hermes-desktop = bin;
+      hermes-desktop = hermesDesktopBinPath;
     };
 
     xdg.desktopEntries.hermes-desktop = {
       name = "Hermes Desktop";
       genericName = "AI Agent Client";
       comment = "Native Electron client for the Hermes Agent gateway";
-      exec = bin;
+      exec = hermesDesktopBinPath;
       icon = pkgs.runCommand "hermes-desktop-icon.png" { } ''
         cp ${inputs.hermes-agent}/apps/desktop/assets/icon.png $out
       '';
